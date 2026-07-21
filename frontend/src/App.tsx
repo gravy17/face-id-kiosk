8c5de8a77fd4ea9c18cd41072131be2041bb3472
Tofi Agbaje <tofarati.agbaje@gmail.com>
2026-07-21 18:26:19 +0100

    routing issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { FactSheet } from '@/pages/FactSheet'
 import { Logs } from '@/pages/Logs'
 import { Register } from '@/pages/Register'
 import { Verify } from '@/pages/Verify'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 export function App() {
   return (
@@ -12,11 +12,11 @@ export function App() {
       <Sidebar />
       <main className="flex-1 overflow-hidden">
         <Routes>
-          <Route path="/"          element={<Navigate to="/register" replace />} />
-          <Route path="/register"  element={<Register  />} />
-          <Route path="/verify"    element={<Verify    />} />
+          <Route path="/" element={<Navigate to="/register" replace />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify" element={<Verify />} />
           <Route path="/factsheet" element={<FactSheet />} />
-          <Route path="/logs"      element={<Logs      />} />
+          <Route path="/logs" element={<Logs />} />
         </Routes>
       </main>
     </div>
